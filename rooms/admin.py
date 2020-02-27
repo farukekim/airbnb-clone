c7806d4ad100d1285f1744232a3970ec5491d1f1
farukekim <21690908@mail.baskent.edu.tr>
2020-02-27 21:51:44 +0300

    Creating room models

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    """ ItemAdmin Definition """
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """ RoomAdmin Definition """
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ PhotoAdmin Definition """
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -1,12 +1,58 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
+
+
+
+class AbstractItem(core_models.TimeStampedModel):
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    """ RoomType Model Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
 
 # Create your models here.
 class Room(core_models.TimeStampedModel):
-
     """ Room Model Definition """
+
     name = models.CharField(max_length=140)
     description = models.TextField()
     country = CountryField()
@@ -20,4 +66,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name

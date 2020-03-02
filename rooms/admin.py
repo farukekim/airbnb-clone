c9ea7875e217fe120dcb411a7224d9335a9d518c
farukekim <21690908@mail.baskent.edu.tr>
2020-03-02 17:03:43 +0300

    Creating room models

--- rooms/admin.py
+++ rooms/admin.py
@@ -11,8 +11,60 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     """ RoomAdmin Definition """
-    pass
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": ("name", "description", "country", "address", "price")
+            }
+        ),
+        (
+            "Times",
+            {
+                "fields": ("check_in", "check_out", "instant_book")
+            }
+        ),
+        (
+            "Spaces",
+            {
+                "fields": ("guests", "beds", "bedrooms", "baths")
+            }
+        ),
+        (
+            "More About the Spaces",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules")
+            }
+        ),
+        (
+            "Last Details",
+            {
+                "fields": ("host",)
+            }
+        ),
+
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("host__superhost","instant_book", "room_type", "amenities", "facilities", "house_rules", "city", "country")
+    search_fields = ("=city", "^host__username")
+    filter_horizontal = ("amenities", "facilities", "house_rules")
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
